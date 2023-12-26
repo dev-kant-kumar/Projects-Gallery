6ae55ed5d59db03ec971b40f6348b80f0d53c304
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-12-26 15:51:58 +0530

    Improve code

--- votingsys.c
+++ votingsys.c
@@ -11,11 +11,10 @@ void voting() {
     char candidate1[] = "Dev Kant Kumar";
     char candidate2[] = "XYZ           ";
     int vote, v1 = 0, v2 = 0, u = 7;
-
-    printf("\n\n");
-
     b:
     while (u > 5) {
+        printf("\n\n");
+        printf("     \033[1;46mWel Come to Voting System \033[0m\n\n");
         printf("\033[1;33mCandidates \t\t\tVotes\033[0m\n");
         printf("\033[5;34m1 - %s \t\t %d\033[0m\n", candidate1, v1);
         printf("\033[5;34m2 - %s \t\t %d\033[0m\n", candidate2, v2);
@@ -23,7 +22,7 @@ void voting() {
         printf("Enter 1 or 2 to vote for your representative \n");
 
         a:
-        printf("Vote : ");
+        printf("\033[1;32mVote : \033[0m");
         scanf("%d", &vote);
 
         switch (vote) {
